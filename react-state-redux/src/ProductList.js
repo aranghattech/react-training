5456bba149dcff7e85adeba40ca3895fa8fad8a3
appsfarmer <appsfarmer@outlook.com>
2023-08-23 15:02:00 +0530

    Added Add to Cart button

--- react-state-redux/src/ProductList.js
+++ react-state-redux/src/ProductList.js
@@ -8,7 +8,14 @@ export default function ProductList() {
         <ul className="list-group">
             {p?.products.map(product => (
                 <li className="list-group-item">
+                   <div className="row">
+                    <div className="col">
                     {product.name} - {product.price}
+                    </div>
+                    <div className="col-auto">
+                        <button type="button" className="btn btn-primary">Add to Cart</button>
+                    </div>
+                   </div>
                 </li>
             ))}
         </ul>
